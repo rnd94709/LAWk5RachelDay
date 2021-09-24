09472ba08daef36b78ccd1e10a16fe16d052acf1
Rachel Day <rnd94709@creighton.edu>
2021-09-24 15:52:08 -0500

    string

--- forms/for/for.js
+++ forms/for/for.js
@@ -1,12 +1,7 @@
 let names = ["Bob", "Janet", "Tom", "Erin", "Lucy", "Alyssa", "Dustin"]
 
-<<<<<<< Updated upstream
-for (i = 0; i<names.length; i++){
-  console.log(i)
-=======
 for (i = 0; i < names.length; i++){
   console.log(`${names[i]}`)
->>>>>>> Stashed changes
   }
   
   
---
+++ forms/strings/strings.js
@@ -0,0 +1,20 @@
+let quoteString = ("Failure is simply the opportunity to begin again this time more intelligently.")
+quoteString = quoteString.toUpperCase()
+
+console.log(`Upper case string is: ${quoteString}`)
+
+let authorString = ("- Henry Ford")
+
+let completeString = quoteString.concat(authorString)
+console.log(`Concatenated string is: ${completeString}`)
+
+let found = completeString.includes("Henry")
+
+if (completeString.includes("Henry"))
+{
+console.log("Henry was in completeString.")
+}
+else
+  {
+  console.log("Henry was NOT in completeString")
+  }
